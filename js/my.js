677700427c3b88611b86741e5dd89c551511c2c8
niuxvdong <158903258@qq.com>
2020-06-17 10:06:08 +0800

    Site updated: 2020-06-17 10:06:05

--- js/my.js
+++ js/my.js
@@ -1,3 +1,3 @@
-var full_page = document.querySelector('#nav.full_page');
+var full_page = document.querySelector('#page-header.full_page');
 
 full_page.style.backgroundImage = 'url(https://img.niuxvdong.top/H21b5f6b8496141a1979a33666e1074d9x.jpg), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url(https://img.niuxvdong.top/H21b5f6b8496141a1979a33666e1074d9x.jpg)';
